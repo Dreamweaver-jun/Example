990c10c44e7450d23f386063f452e3b4b7d12054
Dreamweaver-jun <564171391@qq.com>
2017-02-10 14:26:02 +0800

    hehe

---
+++ js/entry.js
@@ -0,0 +1,3 @@
+require('../css/style.css');                                          //load css
+document.write('It works!');
+document.write(require('../js/module.js'));                           //add module
